03ecc705185c4d8092b8f5cecac7adbf379e289d
Matheus B <matheusbelt@hotmail.com>
2017-10-04 10:57:00 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Requinte</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<script src="assets/js/jquery-3.1.1.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid header">
+		<div class="decoracao"></div>
+		<div class="logo">RR</div>
+		<div class="decoracao"></div>
+	</div> <!-- final do header e container-fluid -->
+	<div class="container-fluid menu-background">
+		<nav class="container menu">
+			<div class="navbar navbar-static-top">
+			<div class="container">
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeader" id="menu-hamburguer">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="collapse navbar-collapse navHeader">
+					<ul class="menu-inicial nav nabar-nav">
+						<a href="#oqbitweek"><li>Eventos</li></a>
+						<a href="#teremos"><li>Depoimentos</li></a>
+						<a href="#footer"><li>Catálogo</li></a>
+						<a href="#footer"><li>Contato</li></a>
+					</ul>
+				</div>
+			</div>
+			</div>
+		</nav>
+	</div><!--  fim do menu-background container-fluid -->
+	<section class="eventos container-fluid" id="eventos">
+		<div class="container">
+			<h1 class="eventos-titulo">Eventos</h1>
+			<article class="casamento">
+				<div class="col-md-6 img-left">
+					<img src="" alt="">
+				</div>
+				<div class="col-md-6 texto-right casamento">
+					<h2 class="eventos-subtitulo casamento-subtitulo">Casamento</h2>
+					<p class="eventos-corpo-texto casamento-corpo-texto">Texto do casamento</p>
+				</div> <!-- primeiro bloco de informação -->
+				<div class="decoracao-laranja"></div>
+				<img src="" alt="Decoração de Casamento">
+				<h3 class="eventos-frase-final">Frase final que determina que o evento acabou</h3>
+				<div class="decoracao-laranja"></div>
+			</article> <!-- fim do casamento (divorcio) -->
+
+			<article class="15anos">
+				<div class="col-md-6 img-left">
+					<img src="" alt="">
+				</div>
+				<div class="col-md-6 texto-right 15anos">
+					<h2 class="eventos-subtitulo 15anos-subtitulo">15 anos</h2>
+					<p class="eventos-corpo-texto 15anos-corpo-texto">Texto do 15 anos</p>
+				</div> <!-- primeiro bloco de informação -->
+				<div class="decoracao-laranja"></div>
+				<img src="" alt="Decoração de 15 anos">
+				<h3 class="eventos-frase-final">Frase final que determina que o evento acabou</h3>
+				<div class="decoracao-laranja"></div>
+			</article> <!-- fim do 15anos (envelhecimento) -->
+
+			<article class="formatura">
+				<div class="col-md-6 img-left">
+					<img src="" alt="">
+				</div>
+				<div class="col-md-6 texto-right formatura">
+					<h2 class="eventos-subtitulo formatura-subtitulo">Formatura</h2>
+					<p class="eventos-corpo-texto formatura-corpo-texto">Texto do Formatura</p>
+				</div> <!-- primeiro bloco de informação -->
+				<div class="decoracao-laranja"></div>
+				<img src="" alt="Decoração de Formatura">
+				<h3 class="eventos-frase-final">Frase final que determina que o evento acabou</h3>
+				<div class="decoracao-laranja"></div>
+			</article> <!-- fim da formatura (ressaca) -->
+		</div> <!-- fim do container -->
+	</section> <!-- fim da section eventos e do container-fluid -->
+
+	<section class="30anos-requinte container-fluid" id="30anos-requinte">
+		<div class="container 30anos-corpo">
+			<h1 class="30anos-titulo">30 Anos Requinte Recepções</h1>
+			<p class="30anos-historia">vem aqui um pouco sobre o requinte e sua história</p>
+			<div class="decoracao-laranja"></div>
+			<div class="col-md-4 30anos-depoimento">
+				<h4 class="30anos-cliente-nome">Luquinhas</h4>
+				<p class="30anos-cliente-depoimento">Depoimento do animal que falou aqui</p>
+			</div>
+			<div class="col-md-4 30anos-decoracao">
+				<img src="" alt="Decoracao">
+			</div>
+			<div class="col-md-4 30anos-depoimento">
+				<h4 class="30anos-cliente-nome">Luquinhas</h4>
+				<p class="30anos-cliente-depoimento">Depoimento do animal que falou aqui</p>
+			</div>
+		</div> <!-- fim do 30anos corpo -->
+	</section><!--  fim de 30 anos requinte -->
+
+	<section class="catalogos container-fluid">
+		<div class="container catalogos-corpo">
+			<h1 class="catalogos-titulo">Catálogos</h1>
+			<div class="catalogo-item col-md-6">
+				<img src="" alt="" class="catalogo-icone">
+				<a href="" download><button class="catalogo-download">Download</button></a>
+			</div><!--  catalogo-item -->
+			<div class="catalogo-item col-md-6">
+				<img src="" alt="" class="catalogo-icone">
+				<a href="" download><button class="catalogo-download">Download</button></a>
+			</div><!--  catalogo-item -->
+		</div> <!-- fim do catalogo-corpo -->
+	</section>
+
+	<footer class="contato container-fluid">
+		<div class="container contato-corpo">
+			<h1 class="contato-titulo">Contato</h1>
+			<div class="col-md-8 contato-mapa">mapa</div>
+			<div class="col-md-4 contato-redes-sociais">icones</div>
+		</div>
+		<div class="copyright text-center">Requinte Recepções [ano], todos os direitos reservados | Desenvolvido por <a href="http://www.facebook.com/elabora.ej/" target="_blank">Elabora - Conteúdo Criativo</a></div>
+	</footer>
+</body>
+</html>
